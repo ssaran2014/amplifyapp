303d26db284c523ed936db77e3d189cb725aae22
Siddharth Saran <siddharth.saran@gmail.com>
2022-02-21 22:37:15 -0500

    Auth, a second time

--- src/App.js
+++ src/App.js
@@ -1,31 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
 import { Amplify } from 'aws-amplify';
-import { Authenticator } from '@aws-amplify/ui-react';
+import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
-function App() {
+function App({ signOut, user }) {
   return (
-    <Authenticator>
-        {({ signOut, user }) => (
-          <div className="App">
-            <main>
-              <header className="App-header">
-                <h1>We now have Auth!</h1>
-              </header>
-              <h1>Hello {user.username}</h1>
-              <button onClick={signOut}>Sign out</button>
-              <img src={logo} className="App-logo" alt="logo" />
-            </main>
-          </div>
-        )}
-    </Authenticator>
+    <div className="App">
+      <h1>Hello {user.username}</h1>
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>We now have Auth!</h1>
+      <button onClick={signOut}>Sign Out</button>
+    </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
